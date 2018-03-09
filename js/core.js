08087d2e59a0feb620693c46b5680b41e3eae060
Filippo <filippo.eng@gmail.com>
2018-03-09 22:12:02 +0000

    changed code

--- js/core.js
+++ js/core.js
@@ -2,16 +2,16 @@ $(document).ready(function () {
 
 	$('#calculate').click(function () {
 
-
 		alert("Hello world 1");
 
 		var val1 = $("#value1").val();
 		var val2 = $("#value2").val();
+
 		alert('Hello ' + val1 + " , " + val2);
 
 		$.ajax({
-			url: "http://www.dalol.org/user_info_handler.php",
-			type: "post",
+			url: 'http://www.dalol.org/user_info_handler.php',
+			type: 'post',
 			dataType: 'json', // this returns JSON
 			data: {
 				value1: val1,
