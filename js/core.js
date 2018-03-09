bd1aba5c79d760475dd14fa7c6c8839f61a8ef0c
Filippo <filippo.eng@gmail.com>
2018-03-09 21:52:56 +0000

    changed code

--- js/core.js
+++ js/core.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
 	$('#calculate').click(function () {
 		var val1 = $("#value1").val();
 		var val2 = $("#value2").val();
-		//alert('Hello ' + val1 + " , " + val2);
+		alert('Hello ' + val1 + " , " + val2);
 
 
 		$.post("http://www.dalol.org/user_info_handler.php",
